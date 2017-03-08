cb1af6cb70b92d7842b9ae0e5e6ca44f6639325a
Erheller <kdwawesome@gmail.com>
2017-03-07 19:13:02 -0600

    Initial commit

---
+++ Assets/Scripts/MoviePlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoviePlayer : MonoBehaviour {
+	MovieTexture mt;	//this is the MovieTexture that is placed on the inside of the sphere
+
+
+	// Use this for initialization
+	void Start () {
+		// this line of code will make the Movie Texture begin playing
+		mt = (MovieTexture)GetComponent<Renderer> ().material.mainTexture;
+		mt.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if (mt.isPlaying)
+				mt.Pause ();
+			else
+				mt.Play ();
+		}
+	}
+}
